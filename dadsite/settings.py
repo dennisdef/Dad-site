087ebebb57ed01849d481b13790ade5c657d4488
dennisdef <dennis.deftereos@gmail.com>
2021-05-11 17:05:18 +0300

    fixed settings

--- dadsite/settings.py
+++ dadsite/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^_0@w2-kdr^(s$y5&i6g$7)n%mu3exs*8!_%c74)*bmbpf##*k'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
